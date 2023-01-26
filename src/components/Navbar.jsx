c4d9444b39ca2cad1208a1383391355a93c5190a
Trinity <amoahtnt6@gmail.com>
2023-01-26 18:35:25 +0000

    Navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import '../style/nav_style.css'
 import Logo from '../assets/icons.svg'
 import { FaChevronDown } from 'react-icons/fa'
-import { AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 
 
 
@@ -11,10 +11,18 @@ import { AiOutlineMenu } from 'react-icons/ai'
 const Navbar = () => {
 
     const [isScrolled, setScrolled] = useState(false)
+    const [isOpen, setOpen] = useState(false)
     window.onscroll = () => {
         setScrolled(window.scrollY === 0 ? false : true)
-        console.log(isScrolled,window.screenY);
+        console.log(isScrolled, window.screenY);
         return () => (window.onscroll == null)
+    }
+
+    const openSideBar = () => {
+        setOpen(true)
+    }
+    const closeSideBar = () => {
+        setOpen(false)
     }
     return (
         <nav style={{ background: isScrolled ? 'var(--primary-dark-color)' : 'transparent' }}>
@@ -23,13 +31,14 @@ const Navbar = () => {
                     <img src={Logo} alt="Logo" />
                 </a>
             </div>
-            <ul>
-                <li> <a href="#">Home</a></li>
-                <li> <a href="#wildlife">Wildlife Africa</a></li>
-                <li> <a href="#reserve">Game Reserve <FaChevronDown /> </a></li>
-                <li> <a href="#contact">Contact Us</a></li>
+            <ul style={{ right: isOpen ? '0' : '-450px' }}>
+                < AiOutlineClose onClick={closeSideBar} />
+                <li onClick={closeSideBar}> <a href="#">Home</a></li>
+                <li onClick={closeSideBar}> <a href="#wildlife">Wildlife Africa</a></li>
+                <li onClick={closeSideBar}> <a href="#reserve">Game Reserve <FaChevronDown /> </a></li>
+                <li onClick={closeSideBar}> <a href="#contact">Contact Us</a></li>
             </ul>
-            <AiOutlineMenu id='menu-btn'/>
+            <AiOutlineMenu id='menu-btn' onClick={openSideBar} />
         </nav>
     )
 }
